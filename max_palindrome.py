0176f77bdb4c7dea916849cf656b24aa8e6cd0f5
flame11st <flame11.st@gmail.com>
2017-09-03 16:48:14 +0300

    added method for return prime numbers

--- max_palindrome.py
+++ max_palindrome.py
@@ -5,3 +5,16 @@ def is_pal(pal):
         if pal[:int(half_len):1] == pal[:int(half_len)-1:-1]: return True
     else:
         if pal[:int(half_len):1] == pal[:int(half_len):-1]: return True
+
+
+#return list with Prime numbers in range x,y
+def primes(x, y):
+    arr = []
+    if x % 2 == 0: x += 1
+    for z in range(x, y, 2):
+        i = 2
+        while i < z:
+            if z % i == 0: break
+            if i == z-1: arr.append(z)
+            i += 1
+    return arr
