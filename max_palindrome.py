b6492ec969afce59fbdf339810c37313b9f8086b
flame11st <flame11.st@gmail.com>
2017-09-03 16:09:49 +0300

    added method for check on the palindrome

---
+++ max_palindrome.py
@@ -0,0 +1,6 @@
+def is_pal(pal):
+    half_len = int(len(pal) / 2.0)
+    if len(pal) % 2 == 0:
+        if pal[:int(half_len):1] == pal[:int(half_len)-1:-1]: return True
+    else:
+        if pal[:int(half_len):1] == pal[:int(half_len):-1]: return True
